2bc9a9eb4250b55c0694297323e1991c52060ef9
IAMLIUBO <imliubo@makingfun.xyz>
2022-01-13 17:26:51 +0800

    add code format script and clean code
    code format rule follow the micropython upstream

--- m5stack/modules/_boot.py
+++ m5stack/modules/_boot.py
@@ -2,13 +2,13 @@ import gc
 import uos as os
 from flashbdev import bdev
 
-uiflow_str = '''
+uiflow_str = """
        _  __ _               
  _   _(_)/ _| | _____      __
 | | | | | |_| |/ _ \ \ /\ / /
 | |_| | |  _| | (_) \ V  V / 
  \__,_|_|_| |_|\___/ \_/\_/  
-'''
+"""
 print(uiflow_str)
 del uiflow_str
 
@@ -18,6 +18,7 @@ try:
         os.mount(vfs, "/flash")
 except OSError:
     import inisetup
+
     vfs = inisetup.setup()
 
 
@@ -25,5 +26,5 @@ gc.collect()
 gc.threshold(56 * 1024)
 
 import micropython
-micropython.alloc_emergency_exception_buf(256)
 
+micropython.alloc_emergency_exception_buf(256)
