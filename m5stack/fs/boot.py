be552ce9b7ee26dd274097efb0c985ebb6bde474
IAMLIUBO <imliubo@makingfun.xyz>
2023-01-14 00:28:54 +0800

    Add new startup ui

--- m5stack/fs/boot.py
+++ m5stack/fs/boot.py
@@ -1,12 +1,52 @@
 # -*- encoding: utf-8 -*-
 # boot.py
 import time
+import binascii
 import esp32
 import network
+import machine
 import M5
 
+
+def show_hits(hits):
+    M5.Lcd.fillRect(24, 70, 95, 11, M5.Lcd.COLOR.BLACK)
+    M5.Lcd.setFont(M5.Lcd.FONTS.DejaVu9)
+    M5.Lcd.drawCenterString(hits, 73, 70)
+
+
+def show_msg(msg):
+    M5.Lcd.fillRect(36, 49, 82, 17, M5.Lcd.COLOR.BLACK)
+    M5.Lcd.setFont(M5.Lcd.FONTS.DejaVu12)
+    M5.Lcd.drawCenterString(msg, 71, 51)
+
+
+def show_ssid(ssid):
+    if len(ssid) > 9:
+        show_msg(ssid[:7] + "...")
+    else:
+        show_msg(ssid)
+
+
+def show_mac():
+    mac = binascii.hexlify(machine.unique_id()).decode('utf-8').upper()
+    M5.Lcd.setFont(M5.Lcd.FONTS.DejaVu9)
+    M5.Lcd.drawCenterString(mac[0:6] + '_' + mac[6:], 65, 85)
+
+
+def show_error(ssid, error):
+    M5.Lcd.clear()
+    M5.Lcd.drawImage("res/img/sys/wifi_err.png", 0, 0)
+    M5.Lcd.drawImage("res/img/sys/mode_dev.png", 0, 98)
+    show_ssid(ssid)
+    show_hits(error)
+    show_mac()
+
+
 M5.begin()
-time.sleep(1)
+M5.Lcd.drawImage("res/img/sys/wifi_ok.png", 0, 0)
+M5.Lcd.drawImage("res/img/sys/mode_dev.png", 0, 98)
+show_mac()
+
 # Read saved Wi-Fi information from NVS
 nvs = esp32.NVS("uiflow")
 ssid = nvs.get_str("ssid0")
@@ -17,11 +57,37 @@ if (len(ssid) > 0) and (len(pswd) > 0):
     wlan.active(False)
     wlan.active(True)
     wlan.connect(ssid, pswd)
-    start = time.time()
-    while wlan.isconnected() is False:
-        time.sleep_ms(200)
-        if (time.time() - start) > 10:
+
+    # show Wi-Fi information
+    show_ssid(ssid)
+
+    # check Wi-Fi connection
+    count = 1
+    while wlan.status() is not network.STAT_GOT_IP:
+        time.sleep_ms(300)
+        if wlan.status() is network.STAT_NO_AP_FOUND:
+            show_error(ssid, "NO AP FOUND")
             break
-    M5.Lcd.print("Local IP: " + str(wlan.ifconfig()[0]))
+        elif wlan.status() is network.STAT_WRONG_PASSWORD:
+            show_error(ssid, "WRONG PASSWORD")
+            break
+        elif wlan.status() is network.STAT_BEACON_TIMEOUT:
+            show_error(ssid, "BEACON TIMEOUT")
+            break
+        elif wlan.status() is network.STAT_ASSOC_FAIL:
+            show_error(ssid, "ASSOC FAIL")
+            break
+        elif wlan.status() is network.STAT_HANDSHAKE_TIMEOUT:
+            show_error(ssid, "HANDSHAKE ERR")
+            break
+        elif wlan.status() is network.STAT_CONNECTING:
+            show_hits('.' * count)
+            count = count + 1
+            if count > 5:
+                count = 1
+
+    if wlan.status() is network.STAT_GOT_IP:
+        show_hits(str(wlan.ifconfig()[0]))
 else:
-    M5.Lcd.print("No Wi-Fi information found, please consider using M5Burner setting.")
+    show_error("Not Found", "Use Burner setup")
+    M5.Lcd.drawImage("res/img/sys/mode_dev.png", 0, 98)
