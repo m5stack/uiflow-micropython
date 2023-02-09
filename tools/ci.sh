ac911d0e942c4c39836b82d71d47ec2eb6a60a19
IAMLIUBO <imliubo@makingfun.xyz>
2023-02-09 14:04:21 +0800

    Update code_formatting.yml

--- tools/ci.sh
+++ tools/ci.sh
@@ -92,9 +92,9 @@ function ci_esp32_idf44_setup {
 
 function ci_esp32_build {
     source esp-idf/export.sh
+    make ${MAKEOPTS} -C m5stack submodules
     make ${MAKEOPTS} -C m5stack littlefs
     make ${MAKEOPTS} -C m5stack mpy-cross
-    make ${MAKEOPTS} -C m5stack submodules
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_8MB
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_SPIRAM_8MB
 
