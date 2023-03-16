c3d2dbdcc03c848e7b9f636ed1bb3c5b75953354
IAMLIUBO <imliubo@makingfun.xyz>
2023-03-16 16:26:33 +0800

    hardware: button callback function support

--- m5stack/components/M5Unified/mpy_m5unified.cpp
+++ m5stack/components/M5Unified/mpy_m5unified.cpp
@@ -19,45 +19,53 @@
 #include <M5UnitRCA.h>
 #include <M5Unified.h>
 
+#include <utility/Button_Class.hpp>
+
 extern "C"
 {
 #include "mpy_m5unified.h"
+#include "mphalport.h"
+
+static void m5_btns_callbacks_init(void);
+static void m5_btns_callbacks_check(void);
 
 /* *FORMAT-OFF* */
-const btn_obj_t m5_btnA    = { {&mp_btn_type},       &(M5.BtnA)          };
-const btn_obj_t m5_btnB    = { {&mp_btn_type},       &(M5.BtnB)          };
-const btn_obj_t m5_btnC    = { {&mp_btn_type},       &(M5.BtnC)          };
-const btn_obj_t m5_btnPWR  = { {&mp_btn_type},       &(M5.BtnPWR)        };
-const btn_obj_t m5_btnEXT  = { {&mp_btn_type},       &(M5.BtnEXT)        };
-const spk_obj_t m5_speaker = { {&mp_spk_type},       &(M5.Speaker)       };
-const pwr_obj_t m5_power   = { {&mp_power_type},     &(M5.Power)         };
-const pwr_obj_t m5_imu     = { {&mp_imu_type},       &(M5.Imu)           };
-      gfx_obj_t m5_display = { {&mp_gfxdevice_type}, NULL                };
+const spk_obj_t m5_speaker = {&mp_spk_type,       &(M5.Speaker)};
+const pwr_obj_t m5_power   = {&mp_power_type,     &(M5.Power)  };
+const pwr_obj_t m5_imu     = {&mp_imu_type,       &(M5.Imu)    };
+      btn_obj_t m5_btnA    = {&mp_btn_type,       &(M5.BtnA)   };
+      btn_obj_t m5_btnB    = {&mp_btn_type,       &(M5.BtnB)   };
+      btn_obj_t m5_btnC    = {&mp_btn_type,       &(M5.BtnC)   };
+      btn_obj_t m5_btnPWR  = {&mp_btn_type,       &(M5.BtnPWR) };
+      btn_obj_t m5_btnEXT  = {&mp_btn_type,       &(M5.BtnEXT) };
+      gfx_obj_t m5_display = {&mp_gfxdevice_type, NULL         };
+
+static btn_obj_t *m5_btn_list[5] = {&m5_btnA, &m5_btnB, &m5_btnC, &m5_btnPWR, &m5_btnEXT};
 /* *FORMAT-ON* */
 
 // TODO: pass configuration parameters
 mp_obj_t m5_begin(void) {
-    auto cfg = M5.config();
     // disable all external display
-    cfg.external_display.module_display = 0;
-    cfg.external_display.atom_display = 0;
-    cfg.external_display.unit_oled = 0;
-    cfg.external_display.unit_lcd = 0;
-    cfg.external_display.unit_rca = 0;
-    cfg.external_display.module_rca = 0;
+    auto cfg = M5.config();
+    cfg.external_display_value = 0;
 
     // initial
     M5.begin(cfg);
 
     M5.Display.clear();
+    // default display
     m5_display.gfx = (void *)(&(M5.Display));
-    // Set default font to DejaVu9, keep same style with UIFlow website UI design.
+    // set default font to DejaVu9, keep same style with UIFlow website UI design.
     M5.Display.setTextFont(&fonts::DejaVu9);
+
+    m5_btns_callbacks_init();
     return mp_const_none;
 }
 
 mp_obj_t m5_update(void) {
     M5.update();
+
+    m5_btns_callbacks_check();
     return mp_const_none;
 }
 
@@ -96,6 +104,63 @@ mp_obj_t m5_setPrimaryDisplayType(mp_obj_t type) {
     M5.setPrimaryDisplayType((m5gfx::board_t)mp_obj_get_int(type));
     m5_display.gfx = (void *)(&(M5.Display));
     return mp_const_none;
+}
+
+/****************************Button Callback handler***************************/
+static void m5_btns_callbacks_init(void) {
+    // clear all callback flag bit
+    for (uint8_t i = 0; i < 5; i++) {
+        m5_btn_list[i]->callbacks.flag = 0;
+    }
+}
+
+static void m5_btns_callbacks_check(void) {
+    // mp_hal_wake_main_task_from_isr();
+    for (uint8_t i = 0; i < 5; i++) {
+        if (m5_btn_list[i]->callbacks.flag) {
+            if (m5_btn_list[i]->callbacks.flag_bit.wasClicked) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasClicked()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasClicked_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+
+            if (m5_btn_list[i]->callbacks.flag_bit.wasSingleClicked) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasSingleClicked()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasSingleClicked_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+
+            if (m5_btn_list[i]->callbacks.flag_bit.wasDoubleClicked) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasDoubleClicked()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasDoubleClicked_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+
+            if (m5_btn_list[i]->callbacks.flag_bit.wasHold) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasHold()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasHold_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+
+            if (m5_btn_list[i]->callbacks.flag_bit.wasPressed) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasPressed()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasPressed_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+
+            if (m5_btn_list[i]->callbacks.flag_bit.wasReleased) {
+                if (((m5::Button_Class *)(m5_btn_list[i]->btn))->wasReleased()) {
+                    mp_sched_schedule(m5_btn_list[i]->callbacks.wasReleased_cb,
+                        mp_obj_new_int(((m5::Button_Class *)(m5_btn_list[i]->btn))->getState()));
+                }
+            }
+        }
+    }
 }
 
 #include "mpy_user_lcd.txt"
