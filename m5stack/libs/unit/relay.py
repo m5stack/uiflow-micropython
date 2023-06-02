97ea6c6c02f2aa76f4dd85b808862626a30b13a4
lbuque <1102390310@qq.com>
2023-06-02 18:34:20 +0800

    fix/unit/relay: fix bug
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/libs/unit/relay.py
+++ m5stack/libs/unit/relay.py
@@ -13,10 +13,10 @@ class Relay:
         self._pin(0)
 
     def value(self, x: bool) -> None:
-        self.value(int(x))
+        self._pin(int(x))
 
     def set_status(self, x: bool) -> None:
-        self.value(int(x))
+        self._pin(int(x))
 
     def get_status(self) -> bool:
-        return bool(self.value())
+        return bool(self._pin())
