3a552ca80a89a7c0877e45e19653a816074f5b3e
pandian.nano <pandian.nano@gmail.com>
2023-07-06 15:12:04 +0800

    feature/unit/lorawan: support the Lorawan 470, 868, 915

---
+++ m5stack/libs/unit/lorawan.py
@@ -0,0 +1,20 @@
+from driver.asr650x import LoRaWAN_470
+import machine
+
+
+class LoRaWAN(LoRaWAN_470):
+    def __init__(self, port):
+        super(LoRaWAN, self).__init__(tx=port[1], rx=port[0])
+        self.tx = port[1]
+        self.rx = port[0]
+
+    def uart_port_id(self, id_num):
+        """
+        set core device uart id
+        id_num: 1-2
+        """
+        self.__uart = machine.UART(id_num, tx=self.tx, rx=self.rx)
+        self.__uart.init(115200, bits=0, parity=None, stop=1)
+
+    def deinit(self):
+        pass
