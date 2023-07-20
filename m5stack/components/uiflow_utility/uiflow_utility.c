aa8f606062465a60e20ab04c2f2c261f74b1b1f0
lbuque <1102390310@qq.com>
2023-07-20 10:34:41 +0800

    feature/uiflow_utility: Support reading and writing of u32 type key-value
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/components/uiflow_utility/uiflow_utility.c
+++ m5stack/components/uiflow_utility/uiflow_utility.c
@@ -22,7 +22,7 @@ bool nvs_read_str_helper(char *ns, char *key, char *value, size_t *_len) {
 
 bool nvs_write_str_helper(char *ns, char *key, char *value) {
     nvs_handle_t nvs_handle;
-    esp_err_t ret = nvs_open("uiflow", NVS_READWRITE, &nvs_handle);
+    esp_err_t ret = nvs_open(ns, NVS_READWRITE, &nvs_handle);
     if (ret != ESP_OK) {
         return false;
     }
@@ -86,5 +86,48 @@ bool nvs_write_u8_helper(char *ns, char *key, uint8_t value) {
     nvs_close(nvs_handle);
 
     ESP_LOGI(TAG, "Write U8 Key: %s Value: %d", key, value);
+    return ret == ESP_OK ? true : false;
+}
+
+bool nvs_read_u32_helper(char *ns, char *key, uint32_t *value) {
+    nvs_handle_t nvs_handle;
+    esp_err_t ret = nvs_open(ns, NVS_READONLY, &nvs_handle);
+    if (ret != ESP_OK) {
+        return false;
+    }
+
+    // Read
+    ret = nvs_get_u32(nvs_handle, key, value);
+
+    // Close
+    nvs_close(nvs_handle);
+
+    ESP_LOGI(TAG, "Read U32 Key: %s Value: %d", key, *value);
+    return ret == ESP_OK ? true : false;
+}
+
+bool nvs_write_u32_helper(char *ns, char *key, uint32_t value) {
+    nvs_handle_t nvs_handle;
+    esp_err_t ret = nvs_open(ns, NVS_READWRITE, &nvs_handle);
+    if (ret != ESP_OK) {
+        return false;
+    }
+
+    // Write
+    ret = nvs_set_u32(nvs_handle, key, value);
+    if (ret != ESP_OK) {
+        return false;
+    }
+
+    // Commit
+    ret = nvs_commit(nvs_handle);
+    if (ret != ESP_OK) {
+        return false;
+    }
+
+    // Close
+    nvs_close(nvs_handle);
+
+    ESP_LOGI(TAG, "Write U32 Key: %s Value: %d", key, value);
     return ret == ESP_OK ? true : false;
 }
