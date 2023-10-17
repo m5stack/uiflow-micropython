0f7677e941b87c779e61990c5ea104354cd4fbb5
lbuque <1102390310@qq.com>
2023-10-17 18:12:42 +0800

    feature: Add M5DIAL board
    Signed-off-by: lbuque <1102390310@qq.com>

---
+++ m5stack/modules/startup/dial/apps/status_bar.py
@@ -0,0 +1,152 @@
+from ..app import AppBase
+from widgets.label import Label
+from widgets.image import Image
+from common.font import MontserratMedium16
+import time
+import M5
+import network
+import uasyncio as asyncio
+from ..res import (
+    SERVER_EMPTY_IMG,
+    SERVER_ERROR_IMG,
+    SERVER_GREEN_IMG,
+    WIFI_DISCONNECTED_IMG,
+    WIFI_EMPTY_IMG,
+    WIFI_GOOD_IMG,
+    WIFI_MID_IMG,
+    WIFI_WORSE_IMG,
+)
+
+
+try:
+    import M5Things
+
+    _HAS_SERVER = True
+except ImportError:
+    _HAS_SERVER = False
+
+
+class NetworkStatus:
+    INIT = 0
+    RSSI_GOOD = 1
+    RSSI_MID = 2
+    RSSI_WORSE = 3
+    DISCONNECTED = 4
+
+
+class CloudStatus:
+    INIT = 0
+    CONNECTED = 1
+    DISCONNECTED = 2
+
+
+_WIFI_STATUS_ICO = {
+    NetworkStatus.INIT: WIFI_EMPTY_IMG,
+    NetworkStatus.RSSI_GOOD: WIFI_GOOD_IMG,
+    NetworkStatus.RSSI_MID: WIFI_MID_IMG,
+    NetworkStatus.RSSI_WORSE: WIFI_WORSE_IMG,
+    NetworkStatus.DISCONNECTED: WIFI_DISCONNECTED_IMG,
+}
+
+_CLOUD_STATUS_ICOS = {
+    CloudStatus.INIT: SERVER_EMPTY_IMG,
+    CloudStatus.CONNECTED: SERVER_GREEN_IMG,
+    CloudStatus.DISCONNECTED: SERVER_ERROR_IMG,
+}
+
+
+class StatusBarApp(AppBase):
+    def __init__(self, icos: dict, wifi) -> None:
+        self._wifi = wifi
+
+    def on_launch(self):
+        self._time_text = self._get_local_time_text()
+        self._network_status = self._get_network_status()
+        self._cloud_status = self._get_cloud_status()
+
+    def on_view(self):
+        self._time_label = Label(
+            "12:23",
+            120,
+            31,
+            w=240,
+            font_align=Label.CENTER_ALIGNED,
+            fg_color=0x534D4C,
+            bg_color=0xEEEEEF,
+            font=MontserratMedium16.FONT,
+        )
+        self._time_label.setText(self._time_text)
+
+        self._network_img = Image(use_sprite=False)
+        self._network_img.set_pos(46, 32)
+        self._network_img.set_size(15, 15)
+        self._network_img.set_src(_WIFI_STATUS_ICO[self._network_status])
+
+        self._cloud_img = Image(use_sprite=False)
+        self._cloud_img.set_pos(179, 32)
+        self._cloud_img.set_size(15, 15)
+        self._cloud_img.set_src(_CLOUD_STATUS_ICOS[self._cloud_status])
+
+    async def on_run(self):
+        while True:
+            t = self._get_local_time_text()
+            if t != self._time_text:
+                self._time_label.setText(t)
+                self._time_text = t
+
+            t = self._get_network_status()
+            if t != self._network_status:
+                self._network_img.set_src(_WIFI_STATUS_ICO[t])
+                self._network_status = t
+
+            t = self._get_cloud_status()
+            if t != self._cloud_status:
+                self._cloud_img.set_src(_CLOUD_STATUS_ICOS[t])
+
+            await asyncio.sleep_ms(5000)
+
+    def _update_time(self, struct_time):
+        self._time_label.setText("{:02d}:{:02d}".format(struct_time[3], struct_time[4]))
+
+    def _update_wifi(self, status):
+        self._wifi_status = status
+        src = _WIFI_STATUS_ICO.get(self._wifi_status)
+        M5.Lcd.drawImage(src.src, src.x, src.y)
+
+    def _update_server(self, status):
+        self._server_status = status
+        src = _CLOUD_STATUS_ICOS.get(self._server_status)
+        M5.Lcd.drawImage(src.src, src.x, src.y)
+
+    @staticmethod
+    def _get_local_time_text():
+        struct_time = time.localtime()
+        return "{:02d}:{:02d}".format(struct_time[3], struct_time[4])
+
+    def _get_network_status(self):
+        status = self._wifi.connect_status()
+        if status is network.STAT_GOT_IP:
+            rssi = self._wifi.get_rssi()
+            if rssi <= -80:
+                return NetworkStatus.RSSI_WORSE
+            elif rssi <= -60:
+                return NetworkStatus.RSSI_MID
+            else:
+                return NetworkStatus.RSSI_GOOD
+        else:
+            return NetworkStatus.DISCONNECTED
+
+    @staticmethod
+    def _get_cloud_status():
+        if _HAS_SERVER is True:
+            status = M5Things.status()
+            return {
+                -2: CloudStatus.DISCONNECTED,
+                -1: CloudStatus.DISCONNECTED,
+                0: CloudStatus.INIT,
+                1: CloudStatus.INIT,
+                2: CloudStatus.CONNECTED,
+                3: CloudStatus.DISCONNECTED,
+            }[status]
+        else:
+            return CloudStatus.DISCONNECTED
---
+++ m5stack/modules/startup/dial/res.py
@@ -0,0 +1,45 @@
+_attrs = {
+    "LOGO_IMG": "/system/dial/boot/boot_logo_1.jpg",
+    "APPLIST_IMG": "/system/dial/page_app_list/page_app_list.jpg",
+    "APPLIST_LEFT_IMG": "/system/dial/page_app_list/left_cursor.jpg",
+    "APPLIST_RIGHT_IMG": "/system/dial/page_app_list/right_cursor.jpg",
+    "RUN_IMG": "/system/dial/page_app_run/page_app_run.jpg",
+    "DEVELOP_PRIVATE_IMG": "/system/dial/page_develop/page_develop_private.jpg",
+    "DEVELOP_PUBLIC_IMG": "/system/dial/page_develop/page_develop_public.jpg",
+    "EZDATA_IMG": "/system/dial/page_ez_data/page_ez_data.jpg",
+    "SETTING_COMMON_IMG": "/system/dial/page_setting_common/page_setting_common.jpg",
+    "BOOT_IMG": "/system/dial/page_setting_common/boot_screen_label.jpg",
+    "BOOT_NO_IMG": "/system/dial/page_setting_common/boot_screen_no.jpg",
+    "BOOT_YES_IMG": "/system/dial/page_setting_common/boot_screen_yes.jpg",
+    "COMX_IMG": "/system/dial/page_setting_common/comx_label.jpg",
+    "COMX_DISABLE_IMG": "/system/dial/page_setting_common/comx_disable.jpg",
+    "COMX_ENABLE_IMG": "/system/dial/page_setting_common/comx_enable.jpg",
+    "SCREEN_IMG": "/system/dial/page_setting_common/screen_brightness_label.jpg",
+    "SCREEN25_IMG": "/system/dial/page_setting_common/screen_brightness_25.jpg",
+    "SCREEN50_IMG": "/system/dial/page_setting_common/screen_brightness_50.jpg",
+    "SCREEN75_IMG": "/system/dial/page_setting_common/screen_brightness_75.jpg",
+    "SCREEN100_IMG": "/system/dial/page_setting_common/screen_brightness_100.jpg",
+    "SETTING_WIFI_IMG": "/system/dial/page_setting_wifi/page_setting_wifi.jpg",
+    "WIFI_DEFAULT_IMG": "/system/dial/page_setting_wifi/wifiServer.jpg",
+    "WIFI_SSID_IMG": "/system/dial/page_setting_wifi/panel_ssid.jpg",
+    "WIFI_PSK_IMG": "/system/dial/page_setting_wifi/panel_pass.jpg",
+    "WIFI_SERVER_IMG": "/system/dial/page_setting_wifi/panel_server.jpg",
+    "SERVER_EMPTY_IMG": "/system/dial/cloud/server_empty.jpg",
+    "SERVER_ERROR_IMG": "/system/dial/cloud/server_error.jpg",
+    "SERVER_GREEN_IMG": "/system/dial/cloud/Server_Green.jpg",
+    "WIFI_DISCONNECTED_IMG": "/system/dial/wifi/wifi_disconnected.jpg",
+    "WIFI_EMPTY_IMG": "/system/dial/wifi/wifi_empty.jpg",
+    "WIFI_GOOD_IMG": "/system/dial/wifi/wifi_good.jpg",
+    "WIFI_MID_IMG": "/system/dial/wifi/wifi_mid.jpg",
+    "WIFI_WORSE_IMG": "/system/dial/wifi/wifi_worse.jpg",
+    "AVATAR_IMG": "/system/common/img/avatar.jpg",
+    "USER_AVATAR_PATH": "/system/common/img/",
+}
+
+
+def __getattr__(attr):
+    value = _attrs.get(attr, None)
+    if value is None:
+        raise AttributeError(attr)
+    globals()[attr] = value
+    return value
