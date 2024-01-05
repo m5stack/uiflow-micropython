a0b329068d8994cd8664a672c689e658ad0bf26b
pandian.nano <pandian.nano@gmail.com>
2024-01-05 09:44:12 +0800

    feature/unit/gesture: sensor recognize up to 9 gestures

---
+++ m5stack/libs/unit/gesture.py
@@ -0,0 +1,35 @@
+from machine import I2C
+from .pahub import PAHUBUnit
+from .unit_helper import UnitError
+from driver.paj7620 import PAJ7620U2
+import time
+import struct
+
+try:
+    from typing import Union
+except ImportError:
+    pass
+
+RAWDATA = 1
+STRING = 2
+
+class GESTUREUnit(PAJ7620U2):
+    def __init__(self, i2c: Union[I2C, PAHUBUnit]) -> None:
+        self.i2c = i2c
+        super(GESTUREUnit, self).__init__(i2c)
+        self._available()
+        self.begin()
+
+    def _available(self) -> None:
+        for i in range(10):
+            time.sleep_ms(100)
+            if (self.i2c_addr in self.i2c.scan()):
+                break
+            if i >= 9:
+                raise UnitError("Gesture unit maybe not connect") 
+    
+    def get_hand_gestures(self):
+        return self.get_gesture()
+    
+        
+    
