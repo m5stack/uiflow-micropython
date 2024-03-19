3f2e3bc6733b7b65fa74dc1651ae40134608ef56
lbuque <1102390310@qq.com>
2024-03-19 15:48:41 +0800

    boards: Add Stamp PICO board.
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/libs/hardware/rgb.py
+++ m5stack/libs/hardware/rgb.py
@@ -35,7 +35,7 @@ class RGB:
         if cls._instance is None:
             if M5.BOARD.M5AtomS3 == board_id:
                 pass
-            elif board_id == M5.BOARD.M5Atom:
+            elif board_id in (M5.BOARD.M5Atom, M5.BOARD.M5StampPico):
                 cls._instance = SK6812(io=27, n=1)
                 return cls._instance
             elif board_id in (M5.BOARD.M5AtomS3Lite, M5.BOARD.M5AtomS3U):
--- m5stack/modules/startup/__init__.py
+++ m5stack/modules/startup/__init__.py
@@ -100,7 +100,7 @@ def startup(boot_opt, timeout: int = 60) -> None:
 
             atoms3 = AtomS3_Startup()
             atoms3.startup(ssid, pswd, timeout)
-        elif board_id == M5.BOARD.M5Atom:
+        elif board_id in (M5.BOARD.M5Atom, M5.BOARD.M5StampPico):
             from .atoms3lite import AtomS3Lite_Startup
 
             atomlite = AtomS3Lite_Startup()
