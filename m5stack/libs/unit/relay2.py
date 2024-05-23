442e45c2c6676fd8d2d00c7b1c24bba20cc379ab
pandian.nano <pandian.nano@gmail.com>
2024-05-23 10:28:42 +0800

    libs/unit/relay2.py: Code modified.
    Signed-off-by: Pandian Nano <pandian.nano@gmail.com>

--- m5stack/libs/unit/relay2.py
+++ m5stack/libs/unit/relay2.py
@@ -6,10 +6,8 @@ from machine import Pin
 
 class Relay2Unit:
     def __init__(self, port: tuple) -> None:
-        self._relay1 = Pin(port[0])
-        self._relay2 = Pin(port[1])
-        self._relay1.init(mode=Pin.OUT)
-        self._relay2.init(mode=Pin.OUT)
+        self._relay1 = Pin(port[0], Pin.OUT)
+        self._relay2 = Pin(port[1], Pin.OUT)
 
     def set_relay_cntrl(self, num: int = 1, control: int = 0) -> None:
         self._relay1(control) if num == 1 else self._relay2(control)
