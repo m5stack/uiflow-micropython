be85167e379ca5de7cde7a178d4d5e96e19e0df5
pandian.nano <pandian.nano@gmail.com>
2024-06-27 11:20:21 +0800

    M5Espnow: Support for modified espnow.
    Signed-off-by: Pandian Nano <pandian.nano@gmail.com>

---
+++ m5stack/libs/m5espnow/__init__.py
@@ -0,0 +1,1 @@
+from .m5espnow import M5ESPNow
---
+++ m5stack/libs/m5espnow/manifest.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+#
+# SPDX-License-Identifier: MIT
+
+package(
+    "m5espnow",
+    (
+        "__init__.py",
+        "m5espnow.py",
+    ),
+    base_path="..",
+    opt=0,
+)
