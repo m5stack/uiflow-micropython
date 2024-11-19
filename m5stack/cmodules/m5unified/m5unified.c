0854f8d390d7cfdf843bc5060e10e82dfe7a3b27
lbuque <1102390310@qq.com>
2024-11-19 16:48:54 +0800

    cmodule/m5unified: Fix missing Als attribute.
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/cmodules/m5unified/m5unified.c
+++ m5stack/cmodules/m5unified/m5unified.c
@@ -100,7 +100,7 @@ static const mp_rom_map_elem_t mp_module_m5_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_Speaker),           MP_OBJ_FROM_PTR(&m5_speaker) },
     { MP_ROM_QSTR(MP_QSTR_Power),             MP_OBJ_FROM_PTR(&m5_power) },
     { MP_ROM_QSTR(MP_QSTR_Imu),               MP_OBJ_FROM_PTR(&m5_imu) },
-    // { MP_ROM_QSTR(MP_QSTR_Als),               MP_OBJ_FROM_PTR(&m5_als) },
+    { MP_ROM_QSTR(MP_QSTR_Als),               MP_OBJ_FROM_PTR(&m5_als) },
     { MP_ROM_QSTR(MP_QSTR_Mic),               MP_OBJ_FROM_PTR(&m5_mic) },
     { MP_ROM_QSTR(MP_QSTR_Widgets),           MP_OBJ_FROM_PTR(&m5_widgets) },
 
