907031b7e3c5d4342229524643552e58cbd17d5c
lbuque <1102390310@qq.com>
2024-11-21 18:13:36 +0800

    cmodule/m5things: Fix compilation errors.
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/cmodules/m5things/modm5things.c
+++ m5stack/cmodules/m5things/modm5things.c
@@ -16,12 +16,12 @@
 extern volatile m5things_status_t m5things_cnct_status;
 extern volatile m5things_info_t m5things_info;
 
-STATIC mp_obj_t m5things_status() {
+static mp_obj_t m5things_status() {
     return mp_obj_new_int(m5things_cnct_status);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(m5things_status_obj, m5things_status);
+static MP_DEFINE_CONST_FUN_OBJ_0(m5things_status_obj, m5things_status);
 
-STATIC mp_obj_t m5things_infos() {
+static mp_obj_t m5things_infos() {
     mp_obj_t tuple[] = {
         mp_obj_new_int(m5things_info.category), // 0
         // mp_obj_new_str((const char *)m5things_info.device_key, strlen((const char *)m5things_info.device_key)),
@@ -33,16 +33,16 @@ STATIC mp_obj_t m5things_infos() {
     };
     return mp_obj_new_tuple(6, tuple);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(m5things_info_obj, m5things_infos);
+static MP_DEFINE_CONST_FUN_OBJ_0(m5things_info_obj, m5things_infos);
 
-STATIC const mp_rom_map_elem_t m5things_globals_table[] = {
+static const mp_rom_map_elem_t m5things_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_M5Things) },
 
     // functions
     { MP_ROM_QSTR(MP_QSTR_status),  MP_ROM_PTR(&m5things_status_obj) },
     { MP_ROM_QSTR(MP_QSTR_info),  MP_ROM_PTR(&m5things_info_obj) },
 };
-STATIC MP_DEFINE_CONST_DICT(mp_module_m5things_globals, m5things_globals_table);
+static MP_DEFINE_CONST_DICT(mp_module_m5things_globals, m5things_globals_table);
 
 // Define module object.
 const mp_obj_module_t m5things_user_cmodule = {
