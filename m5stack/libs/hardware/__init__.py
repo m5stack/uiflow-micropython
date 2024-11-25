2eb800a7566376326d1ade0e09f4d7df193b4df3
lbuque <1102390310@qq.com>
2024-11-25 15:21:51 +0800

    libs/hardware/ir.py: Fix the error when importing hardware in XIAO.
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/libs/hardware/__init__.py
+++ m5stack/libs/hardware/__init__.py
@@ -3,14 +3,8 @@
 # SPDX-License-Identifier: MIT
 
 from machine import *
-from .rgb import *
+from .rgb import RGB
 from .button import Button
-
-try:
-    from .imu import *
-except ImportError:
-    pass
-
 from .ir import IR
 from .rfid import RFID
 from .rotary import Rotary
