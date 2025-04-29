1a6ea7bf1ceee4493fe60171549cb3ad4057ad21
lbuque <lbuque@163.com>
2025-04-29 16:30:16 +0800

    cmodules: Fix compilation errors.
    Signed-off-by: lbuque <lbuque@163.com>

--- m5stack/cmodules/cmodules.cmake
+++ m5stack/cmodules/cmodules.cmake
@@ -4,8 +4,12 @@
 
 # add cdrivers
 include(${CMAKE_CURRENT_LIST_DIR}/cdriver/cdriver.cmake)
+
 # add m5audio2 module
-include(${CMAKE_CURRENT_LIST_DIR}/m5audio2/m5audio2.cmake)
+if(IDF_TARGET STREQUAL "esp32" OR IDF_TARGET STREQUAL "esp32s3")
+    include(${CMAKE_CURRENT_LIST_DIR}/m5audio2/m5audio2.cmake)
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/m5utils/m5utils.cmake)
 
 if (M5_CAMERA_MODULE_ENABLE)
