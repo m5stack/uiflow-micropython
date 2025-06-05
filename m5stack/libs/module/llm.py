13ed667be2ebc0522aa552b2a12ba32a6925d790
LittleMouse <niuchenyang@m5stack.com>
2025-06-05 11:34:42 +0800

    libs/module/llm.py: Add vlm unit.
    Signed-off-by: LittleMouse <niuchenyang@m5stack.com>

--- m5stack/libs/module/llm.py
+++ m5stack/libs/module/llm.py
@@ -304,6 +304,96 @@ class ApiLlm:
         self._on_result = None
 
 
+class ApiVlm:
+    _MODULE_LLM_OK = 0
+    _MODULE_LLM_WAIT_RESPONSE_TIMEOUT = -97
+
+    def __init__(self, module_msg):
+        self._module_msg = module_msg
+        self._llm_work_id = None
+        self._is_msg_finish = None
+        self._on_result = None
+
+    def setup(
+        self,
+        prompt="",
+        model="internvl2.5-1B-364-ax630c",
+        response_format="vlm.utf-8.stream",
+        input="vlm.utf-8",
+        enoutput=True,
+        max_token_len=256,
+        request_id="vlm_setup",
+    ) -> str:
+        cmd = {
+            "request_id": request_id,
+            "work_id": "vlm",
+            "action": "setup",
+            "object": "vlm.setup",
+            "data": {
+                "model": model,
+                "response_format": response_format,
+                "input": input,
+                "enoutput": enoutput,
+                "max_token_len": max_token_len,
+                "prompt": prompt,
+            },
+        }
+        success = self._module_msg.send_cmd_and_wait_to_take_msg(
+            ujson.dumps(cmd), request_id, self._set_llm_work_id, 30000
+        )
+
+        ret_work_id = self._llm_work_id if success else ""
+        self._free_temp()
+        return ret_work_id
+
+    def inference(self, work_id, input_data, request_id="llm_inference") -> str:
+        cmd = {
+            "request_id": request_id,
+            "work_id": work_id,
+            "action": "inference",
+            "object": "vlm.utf-8.stream",
+            "data": {"delta": input_data, "index": 0, "finish": True},
+        }
+        self._module_msg.send_cmd(ujson.dumps(cmd))
+        return self._MODULE_LLM_OK
+
+    def inference_and_wait_result(
+        self, work_id, input_data, on_result, timeout=5000, request_id="llm_inference"
+    ) -> int:
+        self.inference(work_id, input_data, request_id)
+        self._is_msg_finish = False
+        self._on_result = on_result
+
+        start_time = time.ticks_ms()
+        while time.ticks_diff(time.ticks_ms(), start_time) < timeout:
+            self._module_msg.update()
+            if self._module_msg.take_msg(request_id, self._on_inference_result):
+                start_time = time.ticks_ms()
+
+            if self._is_msg_finish:
+                break
+
+        self._free_temp()
+
+        return self._MODULE_LLM_OK
+
+    def _set_llm_work_id(self, msg):
+        if "work_id" in msg:
+            self._llm_work_id = msg["work_id"]
+
+    def _on_inference_result(self, msg):
+        if "data" in msg and "delta" in msg["data"]:
+            if self._on_result:
+                self._on_result(msg["data"]["delta"])
+        if "data" in msg and "finish" in msg["data"]:
+            self._is_msg_finish = msg["data"]["finish"]
+
+    def _free_temp(self):
+        self._llm_work_id = None
+        self._is_msg_finish = None
+        self._on_result = None
+
+
 class ApiAudio:
     _MODULE_LLM_OK = 0
     _MODULE_LLM_WAIT_RESPONSE_TIMEOUT = -97
@@ -795,6 +885,7 @@ class LlmModule:
         self.msg = ModuleMsg(self.comm)
         self.sys = ApiSys(self.msg)
         self.llm = ApiLlm(self.msg)
+        self.vlm = ApiVlm(self.msg)
         self.audio = ApiAudio(self.msg)
         self.tts = ApiTts(self.msg)
         self.melotts = ApiMelotts(self.msg)
@@ -812,6 +903,7 @@ class LlmModule:
 
         # Latest work id and error code
         self.latest_llm_work_id = "llm"
+        self.latest_vlm_work_id = "vlm"
         self.latest_audio_work_id = "audio"
         self.latest_tts_work_id = "tts"
         self.latest_melotts_work_id = "melotts"
@@ -914,6 +1006,34 @@ class LlmModule:
 
     def llm_inference(self, work_id, input_data, request_id="llm_inference") -> str:
         self.latest_error_code = self.llm.inference(work_id, input_data, request_id)
+        return self.latest_error_code
+
+    def vlm_setup(
+        self,
+        prompt="",
+        model="internvl2.5-1B-364-ax630c",
+        response_format="vlm.utf-8.stream",
+        input=None,
+        enoutput=True,
+        enkws=None,
+        max_token_len=256,
+        request_id="vlm_setup",
+    ) -> str:
+        if input is None:
+            input = ["vlm.utf-8"]
+
+        if enkws:
+            if isinstance(input, str):
+                input = [input]
+            input.append(enkws)
+
+        self.latest_vlm_work_id = self.vlm.setup(
+            prompt, model, response_format, input, enoutput, max_token_len, request_id
+        )
+        return self.latest_vlm_work_id
+
+    def vlm_inference(self, work_id, input_data, request_id="vlm_inference") -> str:
+        self.latest_error_code = self.vlm.inference(work_id, input_data, request_id)
         return self.latest_error_code
 
     def audio_setup(
@@ -1123,6 +1243,9 @@ class LlmModule:
 
     def get_latest_llm_work_id(self) -> str:
         return self.latest_llm_work_id
+
+    def get_latest_vlm_work_id(self) -> str:
+        return self.latest_vlm_work_id
 
     def get_latest_audio_work_id(self) -> str:
         return self.latest_audio_work_id
