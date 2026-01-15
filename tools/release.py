7fa1a444d26608e9fd5968b0eb422163b92fac02
lbuque <1102390310@qq.com>
2026-01-15 14:42:00 +0800

    tools/release.py: Add StickS3.
    Signed-off-by: lbuque <1102390310@qq.com>

--- tools/release.py
+++ tools/release.py
@@ -60,6 +60,7 @@ fireware_info_table = (
     FirewareInfo("M5STACK_StickC", "a44718de7ac2c0879b75cd56db8c667e", ""),
     FirewareInfo("M5STACK_StickC_PLUS", "e012605c9ebfebb7159fff2ac35f79c8", ""),
     FirewareInfo("M5STACK_StickC_PLUS2", "39d70217ff0b53c368b0efbfbf2a11af", ""),
+    FirewareInfo("M5STACK_StickS3", "80683b879277c4e1f7cf5ece30b74598", ""),
     FirewareInfo("M5STACK_Tab5", "b11dadb7e93c03daa3a89025f1b76d7a", ""),
     FirewareInfo("M5STACK_Tough", "ffadcb704115b88ca9fd149e567c73f5", "TOUGH"),
     FirewareInfo("M5STACK_Unit_C6L", "9bbb8540dcedf2d7422285c2c077933c", ""),
