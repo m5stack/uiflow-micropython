4cfebc010b7fa47e81f80b86f337f1634cc87eae
lbuque <1102390310@qq.com>
2026-01-16 14:23:04 +0800

    tools/ci.sh: Add DualKey.
    Signed-off-by: lbuque <1102390310@qq.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -357,6 +357,7 @@ function ci_esp32_nightly_build {
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_CoreS3 pack_all
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_Dial pack_all
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_DinMeter pack_all
+    make ${MAKEOPTS} -C m5stack BOARD=M5STACK_DualKey pack_all
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_Fire pack_all
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_NanoC6 pack_all
     make ${MAKEOPTS} -C m5stack BOARD=M5STACK_Paper pack_all
