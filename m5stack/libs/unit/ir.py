8baf2255a9fe296abda9177d4f867a2e70af53b3
Vince Busam <vince@busam.com>
2026-01-17 22:41:11 -0800

    Configure IR Protocol and Timer in IRUnit.

--- m5stack/libs/unit/ir.py
+++ m5stack/libs/unit/ir.py
@@ -1,18 +1,49 @@
 # SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
 #
 # SPDX-License-Identifier: MIT
-from driver.ir.nec import NEC, NEC_8
+from driver.ir.nec import NEC as NEC_TX, NEC_8 as NEC_8_RX, NEC_16 as NEC_16_RX, SAMSUNG as SAMSUNG_RX
+from driver.ir.sony_tx import SONY_ABC as SONY_TX
+from driver.ir.sony_rx import SONY_12 AS SONY_12_RX, SONY_15 as SONY_15_RX, SONY_20 as SONY_20_RX
+from driver.ir.receiver import IR_RX
 import machine
 
 
-class IRUnit:
-    # def __new__(cls, port, proto):
-    #     if proto.upper() == "NEC":
-    #         return NEC(Pin(port[1], Pin.IN))
+class NEC_8:
+    tx = NEC_TX
+    rx = NEC_8_RX
 
-    def __init__(self, port) -> None:
+
+class NEC_16:
+    tx = NEC_TX
+    rx = NEC_8_TX
+
+
+class SAMSUNG:
+    tx = NEC_TX
+    rx = SAMSUNG_RX
+
+
+class SONY_12:
+    tx = SONY_TX
+    rx = SONY_12_RX
+
+
+class SONY_15:
+    tx = SONY_TX
+    rx = SONY_15_RX
+
+
+class SONY_20:
+    tx = SONY_TX
+    rx = SONY_20_RX
+
+
+class IRUnit:
+    def __init__(self, port, ir_cls=NEC_8, timer=1) -> None:
         (self._rx_pin, self._tx_pin) = port
-        self._transmitter = NEC(machine.Pin(self._tx_pin, machine.Pin.OUT, value=0))
+        self._ir_cls = ir_cls
+        self._timer = timer
+        self._transmitter = self._ir_cls.tx(machine.Pin(self._tx_pin, machine.Pin.OUT, value=0))
         self._receiver = None
 
     def tx(self, cmd, data):
@@ -21,4 +52,5 @@ class IRUnit:
     def rx_cb(self, cb):
         if self._receiver:
             self._receiver.close()
-        self._receiver = NEC_8(machine.Pin(self._rx_pin, machine.Pin.IN), cb)
+        IR_RX.Timer_id = self._timer
+        self._receiver = self._ir_cls_rx(machine.Pin(self._rx_pin, machine.Pin.IN), cb)
